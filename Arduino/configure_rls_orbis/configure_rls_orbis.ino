1b8933cf8149d8fcb944ed7ff3be94fdac5da149
HDJGJonas <jonas.haeberli@hispeed.ch>
2023-11-25 10:38:09 +0100

    fix

--- Arduino/configure_rls_orbis/configure_rls_orbis.ino
+++ Arduino/configure_rls_orbis/configure_rls_orbis.ino
@@ -46,7 +46,7 @@ uint16_t readAngle(){
 void setMultiturn(){
   int readVector[] = {0xCD, 0xEF, 0x89, 0xAB, 0x4D, 0x00, 0x00, 0x80, 0x00}; // Set multiturn to 32768 (0x8000) -> this is zero in motorcontroller
   int arraySize = sizeof(readVector) / sizeof(readVector[0]);
-  uint64_t writeMultiturn = spiTransaction(readVector, arraySize);
+  spiTransaction(readVector, arraySize);
 }
 
 void setZeroOffset(){
@@ -60,13 +60,13 @@ void setZeroOffset(){
 
   int readVector[] = {0xCD, 0xEF, 0x89, 0xAB, 0x5A, 0x00, 0x00, highByte, lowByte}; // Set zero offset to recently read encoder position
   int arraySize = sizeof(readVector) / sizeof(readVector[0]);
-  uint64_t writeMultiturn = spiTransaction(readVector, arraySize);
+  spiTransaction(readVector, arraySize);
 }
 
 void safeConfig(){
   int readVector[] = {0xCD, 0xEF, 0x89, 0xAB, 0x63}; // safe the configured settings to non-volatile memory
   int arraySize = sizeof(readVector) / sizeof(readVector[0]);
-  uint64_t writeMultiturn = spiTransaction(readVector, arraySize);
+  spiTransaction(readVector, arraySize);
 }
 
 uint64_t spiTransaction(int SpiCommand[], uint8_t requestedSize) {
